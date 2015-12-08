d9b8a2ebc771e60c186ef8aada0cee6cb109319d
Le Tai <sirlevantai@gmail.com>
2015-12-08 17:20:36 +0700

    Initial commit

---
+++ Debouncer/Debouncer.h
@@ -0,0 +1,19 @@
+//
+//  Debouncer.h
+//  Debouncer
+//
+//  Created by Le Tai on 12/8/15.
+//  Copyright © 2015 levantAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Debouncer.
+FOUNDATION_EXPORT double DebouncerVersionNumber;
+
+//! Project version string for Debouncer.
+FOUNDATION_EXPORT const unsigned char DebouncerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Debouncer/PublicHeader.h>
+
+
---
+++ Debouncer/Debouncer.swift
@@ -0,0 +1,37 @@
+//
+//  Debouncer.swift
+//  Debouncer
+//
+//  Created by Le Tai on 12/8/15.
+//  Copyright © 2015 levantAJ. All rights reserved.
+//
+
+public final class Debouncer {
+    private let delay: Double
+    private var block: (() -> Void)?
+    private var timer: NSTimer?
+    
+    init(delay: Double = 0.25) {
+        self.delay = delay
+    }
+    
+    public func dispatch(block: (() -> Void)) {
+        timer?.invalidate()
+        self.block = block
+        timer = NSTimer.scheduledTimerWithTimeInterval(delay,
+            target: self,
+            selector: callbackSelector,
+            userInfo: nil,
+            repeats: false)
+    }
+    
+    private let callbackSelector = Selector("callback")
+    
+    @objc func callback() {
+        self.block?()
+    }
+    
+    public func invalidate() {
+        timer?.invalidate()
+    }
+}
